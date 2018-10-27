05b340c92ebfc417295d684575d5acf98e37e937
Hayley Hartman <hayleyhartman@Hayleys-MacBook-Pro.local>
2018-10-27 10:44:14 -0400

    h

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,4 +7,30 @@ var friends = require('../data/friends')
 
 //router.get and router.post
 
+router.get("/", function(req, res) {
+
+    // If the main route is hit, then we initiate a SQL query to grab all records.
+    // All of the resulting records are stored in the variable "result."
+    connection.query("SELECT * FROM schools", function(err, result) {
+  
+      // We then begin building out HTML elements for the page.
+      var html = "<h1> Magical Schools </h1>";
+  
+      // Here we begin an unordered list.
+      html += "<ul>";
+  
+      // We then use the retrieved records from the database to populate our HTML file.
+      for (var i = 0; i < result.length; i++) {
+        html += "<li><p> ID: " + result[i].id + "</p>";
+        html += "<p>School: " + result[i].name + " </p></li>";
+      }
+  
+      // We close our unordered list.
+      html += "</ul>";
+  
+      // Finally we send the user the HTML file we dynamically created.
+      res.send(html);
+    });
+  });
+
 module.exports = router
